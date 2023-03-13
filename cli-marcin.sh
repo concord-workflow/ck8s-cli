82520033aae670362ee1ae064dccf1d5c0a1269a
brig <ybrigo@gmail.com>
2023-03-13 19:23:43 +0300

    up

--- cli-marcin.sh
+++ cli-marcin.sh
@@ -1,9 +1,11 @@
+#!/usr/bin/env bash
+
 mvn clean install
 
 java -jar target/quarkus-app/quarkus-run.jar \
 --target-root /tmp/ck8s \
---ck8s-root /Users/marcin.karpezo/projects/ck8s \
---ck8s-ext-root /Users/marcin.karpezo/projects/ck8s-ext \
+--ck8s-root /Users/marcin.karpezo/projects/ck8s-systems/ck8s \
+--ck8s-ext-root /Users/marcin.karpezo/projects/ck8s-systems/ck8s-ext \
 --flow-executor concord-cli \
 --verbose \
 $@
