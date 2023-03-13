0c6946a10e8adebf6dfe4cd14cb5e8c442ef099e
brig <ybrigo@gmail.com>
2023-03-13 14:44:02 +0300

    up

---
+++ cli-marcin.sh
@@ -0,0 +1,8 @@
+mvn clean install
+
+java -jar target/quarkus-app/quarkus-run.jar \
+--target-root /tmp/ck8s \
+--ck8s-root /Users/marcin.karpezo/projects/ck8s \
+--ck8s-ext-root /Users/marcin.karpezo/projects/ck8s-ext \
+--flow-executor concord-cli \
+$@
