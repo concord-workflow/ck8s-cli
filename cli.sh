9dd6dd4c2ac2d38103e3e4c8d94379b12ccc6af1
brig <ybrigo@gmail.com>
2023-04-04 13:09:32 +0200

    cli up

--- cli.sh
+++ cli.sh
@@ -1,1 +1,1 @@
-java -jar target/ck8s-cli-0.1.3-SNAPSHOT-executable.jar --target-root /tmp/ck8s --ck8s-root /Users/brig/prj/github/concord-workflow/ck8s --ck8s-ext-root /Users/brig/prj/github/aetion/ck8s-ext --flow-executor concord-cli $@
+java -jar target/ck8s-cli-0.1.4-SNAPSHOT-executable.jar --target-root /tmp/ck8s --ck8s-root /Users/brig/prj/github/concord-workflow/ck8s --ck8s-ext-root /Users/brig/prj/github/aetion/ck8s-ext --flow-executor concord-cli $@
