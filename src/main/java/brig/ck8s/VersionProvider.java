3d6341594b9b02ab712eb66fa49b2730059e811b
brig <ybrigo@gmail.com>
2023-04-04 16:41:22 +0200

    dump java version

--- src/main/java/brig/ck8s/VersionProvider.java
+++ src/main/java/brig/ck8s/VersionProvider.java
@@ -1,8 +1,12 @@
 package brig.ck8s;
 
+import brig.ck8s.utils.CliCommand;
 import picocli.CommandLine;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class VersionProvider implements CommandLine.IVersionProvider {
@@ -15,6 +19,16 @@ public class VersionProvider implements CommandLine.IVersionProvider {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return new String[]{props.getProperty("project.version")};
+        return new String[]{"ck8s-cli version: " + props.getProperty("project.version"), javaVersion()};
+    }
+
+    private static String javaVersion() {
+        try {
+            CliCommand.Result result = CliCommand.saveOut(Arrays.asList("java", "--version"), Path.of(System.getProperty("user.dir")))
+                    .execute();
+            return result.stdout();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get java version: " + e.getMessage());
+        }
     }
 }
