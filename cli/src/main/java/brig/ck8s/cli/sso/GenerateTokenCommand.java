e7bd05a76aea663930dc8886d3db522673fe40fe
brig <ybrigo@gmail.com>
2023-07-24 17:46:55 +0200

    token command info

--- cli/src/main/java/brig/ck8s/cli/sso/GenerateTokenCommand.java
+++ cli/src/main/java/brig/ck8s/cli/sso/GenerateTokenCommand.java
@@ -196,11 +196,12 @@ public class GenerateTokenCommand
             return null;
         }
 
+        LogUtils.info("Attempting to automatically open the SSO authorization page in your default browser.");
+        LogUtils.info("If the browser does not open or you wish to use a different device to authorize this request, open the following URL:");
+        LogUtils.info(verificationUrl);
+
         if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
             Desktop.getDesktop().browse(new URI(verificationUrl));
-        }
-        else {
-            LogUtils.warn("Could not open default browser. Open link manually: {}", verificationUrl);
         }
 
         long startedAt = System.currentTimeMillis();
