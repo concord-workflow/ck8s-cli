8f8447d0fd1d3fb7f82e04ae32ed8f0aa45096ac
brig <ybrigo@gmail.com>
2024-11-05 13:26:03 -0500

    tests cleanup

--- cli/src/test/java/brig/ck8s/cli/Ck8sCliTest.java
+++ cli/src/test/java/brig/ck8s/cli/Ck8sCliTest.java
@@ -7,14 +7,6 @@ import static brig.ck8s.cli.Ck8sCliAssertions.*;
 public class Ck8sCliTest
 {
     @Test
-    public void testClusterList()
-    {
-//        assertSuccess("-l")
-//                .assertOutContainsMatchingLine(
-//                        "^\s+Alias\s+Name\s+Region\s+Server\s+Path\s+$");
-    }
-
-    @Test
     public void testFailOnListAndActionAtSameTime()
     {
         assertFailed("-l -a dnsmasq-setup")
@@ -39,87 +31,6 @@ public class Ck8sCliTest
                 .assertExitCode(2)
                 .assertErrContainsMatchingLine(
                         "^Error: --flow=<flow>, -a=<actionType> are mutually exclusive \\(specify only one\\)$");
-    }
-
-    @Test
-    public void testRunUpAction()
-    {
-        assertRunAction("up", "ck8sUp");
-    }
-
-    @Test
-    public void testRunDownAction()
-    {
-        assertRunAction("down", "ck8sDown");
-    }
-
-    @Test
-    public void testRunDockerRegistryAction()
-    {
-        assertRunAction("docker-registry", "ck8sDockerRegistry");
-    }
-
-    @Test
-    public void testRunDnsMasqSetupAction()
-    {
-        assertRunAction("dnsmasq-setup", "dnsmasqSetup");
-    }
-
-    @Test
-    public void testRunDnsMasqRestartAction()
-    {
-        assertRunAction("dnsmasq-restart", "dnsmasqRestart");
-    }
-
-    @Test
-    public void testRunInstallConcordAction()
-    {
-        assertRunAction("install-concord", "installConcord");
-    }
-
-    @Test
-    public void testRunReinstallConcordAction()
-    {
-        assertRunAction("reinstall-concord-agent-pool", "reinstallConcordAgentPool");
-    }
-
-    @Test
-    public void testRunCk8sConsoleAction()
-    {
-        assertRunAction("console", "ck8sConsole");
-    }
-
-    @Test
-    public void testRunAwsKubeConfAction()
-    {
-        assertRunAction("aws-kube-config", "awsKubeconfig");
-    }
-
-    @Test
-    public void testLocalCluster()
-    {
-        assertSuccess("-c local -f cluster --dry-run "
-                + "--ck8s-root ./src/test/resources/ck8s")
-                .assertOutContainsMatchingLine("Executing action: ck8sDown")
-                .assertOutContainsMatchingLine("Executing action: ck8sUp")
-                .assertOutContainsMatchingLine("Executing action: assertLocalCluster");
-    }
-
-    @Test
-    public void testRunFlow()
-    {
-        assertSuccess("-f show -c local -p default --dry-run "
-                + "--ck8s-root ./src/test/resources/ck8s")
-                .assertOutContainsMatchingLine(
-                        "Running flow: show on cluster: local with profile: default");
-    }
-
-    @Test
-    public void testTokenOperation()
-    {
-        assertSuccess("--dry-run token -p default")
-                .assertOutContainsMatchingLine(
-                        "Generating OIDC token for profile: default");
     }
 
     @Test
